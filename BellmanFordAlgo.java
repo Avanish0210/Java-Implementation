09243bf768de16ac199898809be2f5a102f2cae0
Avanish0210 <avanishsingh0210@gmail.com>
2026-03-24 23:13:44 +0530

    Cycle check

--- BellmanFordAlgo.java
+++ BellmanFordAlgo.java
@@ -49,6 +49,19 @@ public class BellmanFordAlgo {
             }
         }
 
+        //for detect negative weight cycle
+        for(int i=0 ; i<V ; i++){
+                for(int j=0 ; j<graph[i].size() ; j++){
+                    Edge e = graph[i].get(j);
+                    int u = e.src;
+                    int v = e.dest;
+                    if(dist[u] != Integer.MAX_VALUE && dist[u]+e.wt < dist[v]){
+                        System.out.println("negative weight cycle");
+                    }
+                }
+            }
+        
+
         for(int i=0 ; i<dist.length ; i++){
             System.out.print(dist[i]+" ");
         }
